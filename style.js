81b0e41535ecb48b92bdfcfb9655de96c9417edf
SIMOCCAUCH30 <simoccauch30@SIMOCCAUCH30.lan>
2018-06-12 09:57:58 +0200

    Création EXO en groupe

---
+++ style.js
@@ -0,0 +1,43 @@
+ /*
+ console.log("salut");
+ var nom = "Dupont";
+ var prénom = "David";
+ var ville = "Auch";
+
+ var result = nom + prenom + ville;
+ alert(result); 
+
+
+
+
+
+console.log("salut");
+var nom = "Dupont";
+var prénom = "David";
+var ville = "Auch";
+
+    var saisie = prompt();
+    alert("Boujour\n" + "nom: " + saisie); 
+
+
+
+
+
+var NOM  = prompt('Entrez le nom :');
+var PRENOM = prompt('Entrez le prenom :');
+var VILLE = prompt('Entrez la ville :');
+    //var result = NOM + PRENOM + VILLE; (Resultat qui fonctionne aussi)
+    alert("Nom: " + (NOM) + "\n" + "Prénom: "  + (PRENOM) + "\n" + "Ville: " + (VILLE));
+    alert(result); 
+
+
+*/
+
+
+
+
+var nb1 = (4);
+var nb2 = (4);
+ var result = nb1 * nb2 ;
+alert(result); 
+ 
